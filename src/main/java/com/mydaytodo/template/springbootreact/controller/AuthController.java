35d8b614fa67b196280371cce145f00540ac4cee
Avyay <shubhamsinghak1234@gmail.com>
2024-09-24 21:53:31 +0530

    add-google-auth

---
+++ src/main/java/com/mydaytodo/template/springbootreact/controller/AuthController.java
@@ -0,0 +1,13 @@
+package com.mydaytodo.template.springbootreact.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
